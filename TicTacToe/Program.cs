a14cb2313c2f1edd055473d084b3c0d4cb867a98
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-08 14:36:55 +0530

    [Girish]Displaying the Game Board

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace TicTacToe
 {
@@ -8,7 +10,9 @@ namespace TicTacToe
         {
             Console.WriteLine("Welcome To Tic Tac Toe Problem!!!");
             char[] board = gameBoard();
-
+            char person = TicTacToeGame.GetPersonInput();
+            char computer = TicTacToeGame.GetComputerInput(person);
+            Console.WriteLine("User has chosen {0} and computer has chosen {1}", person, computer);
         }
         public static char[] gameBoard()
         {
@@ -17,9 +21,16 @@ namespace TicTacToe
             {
                 board[i] = ' ';
             }
+                Console.WriteLine("   |    |  ");
+                Console.WriteLine(" {0} | {1}  |{2} ", board[1], board[2], board[3]);
+                Console.WriteLine("___|____|___");
+                Console.WriteLine("   |    |  ");
+                Console.WriteLine(" {0} | {1}  |{2} ", board[4], board[5], board[6]);
+                Console.WriteLine("___|____|___");
+                Console.WriteLine("   |    |  ");
+                Console.WriteLine(" {0} | {1}  |{2} ", board[7], board[8], board[9]);
+                Console.WriteLine("   |    |  ");
             return board;
         }
-        
-        
     }
 }
--- TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToeGame.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace TicTacToe
 {
-    class PlayerInput
+    class TicTacToeGame
     {
+        public static char GetPersonInput()
+        {
+            char person;
+            Console.WriteLine("Enter Your choice X or O");
+            person = Convert.ToChar(Console.ReadLine());
+            return person;
+        }
+        public static char GetComputerInput(char human)
+        {
+            char computer;
+            if (human == 'x' || human == 'X')
+                computer = 'O';
+            else
+            {
+                computer = 'X';
+            }
+            return computer;
+        }
     }
 }
